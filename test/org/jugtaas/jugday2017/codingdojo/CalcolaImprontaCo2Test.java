173baad4d98ecf68520159b8f028089041378973
Mario A. Santini <alexmario74@gmail.com>
2017-03-07 23:16:20 +0100

    aggiunta classe test d'esempio

---
+++ test/org/jugtaas/jugday2017/codingdojo/CalcolaImprontaCo2Test.java
@@ -0,0 +1,15 @@
+package org.jugtaas.jugday2017.codingdojo;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by mario on 07/03/2017.
+ */
+public class CalcolaImprontaCo2Test {
+    @Test
+    public void oneTest() {
+        assertThat("Just a test sample", false);
+    }
+}
